7dff3be77377cbc704d059afd9c9aca07c501caa
vlakshmilavanya <v.lakshmilavanya5@gmail.com>
2022-03-12 21:22:32 +0530

    Done :Register, Login, Logout

---
+++ OnlineVotingSystem/OnlineVotingSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,224 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using OnlineVotingSystem.Areas.Identity.Data;
+
+namespace OnlineVotingSystem.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<OnlineVotingSystemUser> _signInManager;
+        private readonly UserManager<OnlineVotingSystemUser> _userManager;
+        private readonly IUserStore<OnlineVotingSystemUser> _userStore;
+        private readonly IUserEmailStore<OnlineVotingSystemUser> _emailStore;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private IWebHostEnvironment WebHostEnvironment;
+
+        public RegisterModel(
+            UserManager<OnlineVotingSystemUser> userManager,
+            IUserStore<OnlineVotingSystemUser> userStore,
+            SignInManager<OnlineVotingSystemUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender, IWebHostEnvironment webHostEnvironment)
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            public string Name { get; set; }
+
+            public Int64 Mobile { get; set; }
+
+            public DateTime DateOfBirth { get; set; }
+            public string State { get; set; }
+            public string City { get; set; }
+            public string Address { get; set; }
+            public IFormFile ProfileImage { get; set; }
+
+        }
+
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                IFormFile file = Input.ProfileImage;
+                string ImagePath = uploadImage(file);
+                var user = new OnlineVotingSystemUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    Name = Input.Name,
+                    City = Input.City,
+                    State = Input.State,
+                    Address = Input.Address,
+                    DateOfBirth = Input.DateOfBirth,
+                    ProfileImage = ImagePath,
+                    IsVoter = false
+
+                };
+
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
+        private string uploadImage(IFormFile file)
+        {
+            string fileName = null;
+            if(file != null)
+            {
+                string uploadDir = Path.Combine(WebHostEnvironment.WebRootPath, "Images");
+                fileName = Guid.NewGuid().ToString() + "-" + file.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+
+        private OnlineVotingSystemUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<OnlineVotingSystemUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(OnlineVotingSystemUser)}'. " +
+                    $"Ensure that '{nameof(OnlineVotingSystemUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+
+        private IUserEmailStore<OnlineVotingSystemUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<OnlineVotingSystemUser>)_userStore;
+        }
+    }
+}
---
+++ OnlineVotingSystem/OnlineVotingSystem/Models/RoleInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineVotingSystem.Areas.Identity.Data;
+using OnlineVotingSystem.Data;
+
+namespace OnlineVotingSystem.Models
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(OnlineVotingSystemContext context, IServiceProvider serviceProvider,
+            UserManager<OnlineVotingSystemUser> userManager)
+        {
+            var Role = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Voter", "Candidate" };
+            IdentityResult result;
+            foreach(var name in roleNames)
+            {
+                var roleExists = await Role.RoleExistsAsync(name);
+                if(!roleExists)
+                {
+                    result = await Role.CreateAsync(new IdentityRole(name));
+                }
+            }
+
+            string Email = "admin@gmail.com";
+            string Password = "Admin@123";
+            if(userManager.FindByEmailAsync(Email).Result == null)
+            {
+                OnlineVotingSystemUser user = new OnlineVotingSystemUser();
+                user.UserName = Email;
+                user.Email = Email;
+                IdentityResult identityResult = userManager.CreateAsync(user, Password).Result;
+
+                if(identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+         }
+    }
+}
